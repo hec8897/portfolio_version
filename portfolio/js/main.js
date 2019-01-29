bab77c21c1ff8cbb6ac9240ff6f0768b6de74e6c
hec8897 <37240562+hec8897@users.noreply.github.com>
2019-01-29 22:49:37 +0900

    Add files via upload

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -44,7 +44,7 @@ var eventMethod={
                 },function(){
                     $(".im_list_li").css({"color":"black"});
                     $("#improfile3").css({"color":"white"});
-                    alert("준비중입니다")
+                    alert("준비중입니다");
                 });              
             })
             $("#improfile4").click(function(){
@@ -53,7 +53,7 @@ var eventMethod={
                 },function(){
                     $(".im_list_li").css({"color":"black"});
                     $("#improfile4").css({"color":"white"});
-                    alert("준비중입니다")
+                    alert("준비중입니다");
                 });              
             })
         })                                 
